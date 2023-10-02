ac59200902186d9a73800a9e5134e786bda96fb3
Roman Akhmetov <romanbackof@gmail.com>
2023-10-03 00:04:12 +0300

    some swap chain implementation

--- Engine/GraphicsEngine/SwapChain/SwapChain.h
+++ Engine/GraphicsEngine/SwapChain/SwapChain.h
@@ -5,16 +5,18 @@
 #ifndef MEINENGINE_SWAPCHAIN_H
 #define MEINENGINE_SWAPCHAIN_H
 
-
+#include <d3d11.h>
 class SwapChain {
 public:
     SwapChain();
     ~SwapChain();
 
     //initialize swapchain for a window
-    bool init();
+    bool init(HWND hwnd, UINT width, UINT height);
     //release the swapchain
     bool release();
+private:
+    IDXGISwapChain* m_swap_chain;
 };
 
 
